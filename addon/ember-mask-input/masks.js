f29b7f59c1bc42d488359bb98357f5958677d529
Ahmet Emre Kılınç <aekilinc@gmail.com>
2018-04-11 11:13:31 +0300

    iban can have letters

--- addon/ember-mask-input/masks.js
+++ addon/ember-mask-input/masks.js
@@ -1,6 +1,6 @@
 
 let MASK_MAPPING = {
-  'iban':'AA00 0000 0000 0000 0000 0000 00',
+  'iban':'AAAA AAAA AAAA AAAA AAAA AAAA AA',
   'phone-no':'(000) 000 00 00',
   'credit-card':'0000 0000 0000 0000'
 };
