c235e663ba51e904dd9ecdfc0b5be3c289fd5f50
Ahmet Emre Kılınç <aekilinc@gmail.com>
2018-04-11 14:03:35 +0300

    iban max length is set 34

--- addon/ember-mask-input/masks.js
+++ addon/ember-mask-input/masks.js
@@ -1,6 +1,6 @@
 
 let MASK_MAPPING = {
-  'iban':'AAAA AAAA AAAA AAAA AAAA AAAA AA',
+  'iban':'AAAA AAAA AAAA AAAA AAAA AAAA AAAA AAAA AA',
   'phone-no':'(000) 000 00 00',
   'credit-card':'0000 0000 0000 0000'
 };
